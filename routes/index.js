b80f3806f24d52fe57849e66b297c49018c736fc
Jose A. Bentez Pelez <jabenitezz@gmail.com>
2015-06-18 10:42:07 +0200

    Pagina de autor

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,12 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+
+/* GET home author. */
+router.get('/author', function(req, res) {
+  res.render('author', { title: 'Quiz' });
+});
+
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
